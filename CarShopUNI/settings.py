d1833721b332d5648dc12cefb85c03d728e5a348
tBogoyski <tano.bogoyski@protonmail.com>
2022-09-23 11:12:02 +0300

    Implement .gitignore

--- CarShopUNI/settings.py
+++ CarShopUNI/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'cars.apps.CarsConfig',
-    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
