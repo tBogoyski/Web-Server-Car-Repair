69357fc4bc4e1fe7c930b783ddc7abfddf65e05f
tBogoyski <tano.bogoyski@protonmail.com>
2022-09-24 21:45:42 +0300

    Make index work

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cars.forms import CarForm
+
+
+def index(request):
+    if request.method == 'POST':
+        form = CarForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CarForm()
+
+    return render(request, "index.html", {'form': form})
