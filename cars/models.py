f8c724cdb961add77118816387bdd6177500cd1c
Tano Bogoyski <80518940+tBogoyski@users.noreply.github.com>
2022-09-24 20:21:08 +0300

    Implement models (#1)

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    maker = models.CharField(max_length=100)
+    year_of_make = models.DateField()
+    registration_number = models.CharField(max_length=8)
+
+
+class Event(models.Model):
+    date_of_event = models.DateTimeField()
+    km_of_car = models.IntegerField()
+    event_text = models.TextField()
+    periodic_event = models.BooleanField()
+    next_date = models.DateField()
+    next_change = models.DateField()
