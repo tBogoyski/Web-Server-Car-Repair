ee9abe3ca156ba7e95d177b97580bcb7860c3305
tBogoyski <tano.bogoyski@protonmail.com>
2022-09-25 22:55:50 +0300

    Implement delete and update car functionality

--- cars/views.py
+++ cars/views.py
@@ -1,23 +1,19 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView, CreateView, TemplateView
 
 from cars.forms import CarForm
 from cars.models import Car
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
 
-def add_car(request):
-    if request.method == 'POST':
-        form = CarForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = CarForm()
-
-    return render(request, 'add-car.html', {'form': form})
+class CarCreateView(CreateView):
+    model = Car
+    form_class = CarForm
+    template_name = 'add-car.html'
+    success_url = reverse_lazy('car_list')
 
 
 class CarListView(ListView):
@@ -28,3 +24,16 @@ class CarListView(ListView):
 class CarDetailView(DetailView):
     model = Car
     template_name = 'car-details.html'
+
+
+class CarDeleteView(DeleteView):
+    model = Car
+    success_url = reverse_lazy('car_list')
+    template_name = 'delete-car.html'
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarForm
+    template_name = 'edit-car.html'
+    success_url = reverse_lazy('car_list')
