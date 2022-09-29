5ec8a249d9bec2f53d9b77102453a5fc9549f61a
tBogoyski <tano.bogoyski@protonmail.com>
2022-09-29 22:36:06 +0300

    Make index pretty and implement navigation

--- CarShopUNI/settings.py
+++ CarShopUNI/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'CarShopUNI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'cars/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'cars/static')
 STATIC_URL = '/static/'
 
 # Default primary key field type
