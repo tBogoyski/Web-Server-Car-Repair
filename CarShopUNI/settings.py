620bc34f84d6ec6a69beedc886c4535e6e4b0f51
tBogoyski <tano.bogoyski@protonmail.com>
2022-09-29 21:08:11 +0300

    Add bootstrap to the project

--- CarShopUNI/settings.py
+++ CarShopUNI/settings.py
@@ -110,14 +110,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
-LOGIN_REDIRECT_URL = 'home'
-LOGIN_URL = 'login'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
